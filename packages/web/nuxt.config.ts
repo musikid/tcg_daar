416c278af7a308464b3f8d4a4a1a1f409b413307
Sayafdine Said <musikid@outlook.com>
2023-10-26 18:25:13 +0200

    feat(web/frontend): add prisma

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,5 +1,35 @@
+import { presetIcons, presetWebFonts } from 'unocss'
+import presetUno from '@unocss/preset-uno'
+import { presetDaisy } from 'unocss-preset-daisy'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@unocss/nuxt'],
+  css: ['@unocss/reset/tailwind.css'],
+  postcss: {},
+  runtimeConfig: {
+    public: {
+      chain: {
+        network: 'localhost',
+        chainId: 31337,
+      }
+    }
+  },
+  unocss: {
+    presets: [
+      presetUno(),
+      presetDaisy({
+        styled: false,
+      }),
+      presetIcons(),
+      presetWebFonts({
+        fonts: {
+          serif: ['Rokkitt'],
+          sans: ['Titillium Web'],
+          pixel: ['Handjet']
+        }
+      }),
+    ],
+  }
 })
